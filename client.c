8517f8d9e4e350fc7bcc8c14b31b03c9a42640b7
NeksHinto <nhinojo@itba.edu.ar>
2022-06-19 20:11:00 -0300

    client fix

--- client.c
+++ client.c
@@ -1,18 +1,22 @@
 // Client side C/C++ program to demonstrate Socket
-// programming
+#include "desafios.h"
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #define PORT 8080
+#define MAX_BUFFER 1024
+#define SA struct sockaddr
 
 int main(int argc, char const *argv[])
 {
-    int sock = 0, valread, client_fd;
+    int sock = 0, client_fd;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
+    char buffer[MAX_BUFFER];
+    int buffIndex = 0;
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -22,8 +26,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary
-    // form
+    // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
         printf(
@@ -37,12 +40,20 @@ int main(int argc, char const *argv[])
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(sock, buffer, 1024);
-    printf("%s\n", buffer);
 
-    // closing the connected socket
-    close(client_fd);
-    return 0;
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        buffer[buffIndex++] = c;
+        if(c == '\n')
+        {
+            buffer[buffIndex] = 0;
+            if (send(sock, buffer, strlen(buffer), 0) < 0)
+            {
+                HANDLE_ERROR("Error sending from client.\n");
+            }
+            buffIndex = 0;
+        }
+    }
+    return EXIT_SUCCESS;
 }
