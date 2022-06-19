9a62250bf200001a8e5d8b2f605b7b6412ffb056
NeksHinto <nhinojo@itba.edu.ar>
2022-06-19 20:10:53 -0300

    server fix

--- server.c
+++ server.c
@@ -1,5 +1,9 @@
 // Server side C/C++ program to demonstrate Socket
-// programming
+/*
+    Implementación sacada de: https://www.geeksforgeeks.org/socket-programming-cc/
+*/
+#include "desafios.h"
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,27 +13,23 @@
 #define PORT 8080
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+      HANDLE_ERROR("Error in socket");
     }
 
     // Forcefully attaching socket to the port 8080
     if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   SO_REUSEADDR, &opt,
                    sizeof(opt)))
     {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+      HANDLE_ERROR("Error in setsockopt");
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
@@ -39,28 +39,35 @@ int main(int argc, char const *argv[])
     if (bind(server_fd, (struct sockaddr *)&address,
              sizeof(address)) < 0)
     {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+      HANDLE_ERROR("Error in bind");
     }
     if (listen(server_fd, 3) < 0)
     {
-        perror("listen");
-        exit(EXIT_FAILURE);
+      HANDLE_ERROR("Error in listen");
     }
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                              (socklen_t *)&addrlen)) < 0)
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
+      HANDLE_ERROR("Error in accept");
     }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
 
-    // closing the connected socket
-    close(new_socket);
-    // closing the listening socket
-    shutdown(server_fd, SHUT_RDWR);
-    return 0;
+    FILE *fp;
+    fp = fdopen(new_socket, "r");
+    if (fp == NULL)
+    {
+        HANDLE_ERROR("Error in fdopen.");
+    }
+    init(fp);
+    int completed = 1;
+    while (completed <= CALLENGE_COUNT)
+    {
+        if(challenge(completed) > 0){
+            completed++;
+        }
+        sleep(2);
+    }
+    // Liberamos los recursos utilizados.
+    end();
+    close(server_fd);
+    return EXIT_SUCCESS;
 }
